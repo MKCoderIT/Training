c9f3128fed149077f8124866229d7c009bf88c24
Mohammad Kamyar <kamyar.taslimi.1385@gmail.com>
2025-04-09 14:18:47 +0330

    learn and practice javascript prototypes

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
 "use strict";
 
-var array = [1,2,3,4,5,6];
+// var array = [1,2,3,4,5,6];
 
 
-function totalArray(arr){
-    var total = 0;
+// function totalArray(arr){
+//     var total = 0;
 
-    // for
-    for (let i = 0; i < arr.length; i++) {
-    total += arr[i];
-    }
+//     // for
+//     for (let i = 0; i < arr.length; i++) {
+//     total += arr[i];
+//     }
 
-    //while
-    var a = 0;
-    while(a < arr.length){
-    total += arr[a];a++;
-    }
+//     //while
+//     var a = 0;
+//     while(a < arr.length){
+//     total += arr[a];a++;
+//     }
 
-    //forEach
-    arr.forEach((item) => {
-        total += item;
-    });
+//     //forEach
+//     arr.forEach((item) => {
+//         total += item;
+//     });
 
-    //reduce
-    return arr.reduce((total,item) => total += item,0)
+//     //reduce
+//     return arr.reduce((total,item) => total += item,0)
 
-    return total;
-}
+//     return total;
+// }
 
-console.log(totalArray(array));
+// console.log(totalArray(array));
+
+// function prototypeExample2(Names){
+//     this.name = Names;
+// }
+
+// function prototypeExample(Names){
+// this.name = Names;
+// }
+// prototypeExample.prototype.data = new prototypeExample2("example");
+
+// var test1 = new prototypeExample("ali");
+// var test2 = new prototypeExample("reza");
+
+
+
+// console.log(test1, test2);
+// console.log(test1.data === test2.data);
 
