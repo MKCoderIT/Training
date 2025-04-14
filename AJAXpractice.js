0d95571a785ee44f342d8b4169c2a75845996ff4
Mohammad Kamyar <kamyar.taslimi.1385@gmail.com>
2025-04-15 01:34:45 +0330

    Fix bug in GET AJAX method, add more options for GET AJAX method, and develop POST AJAX method

--- AJAXpractice.js
+++ AJAXpractice.js
@@ -20,18 +20,28 @@ if(UserUnorderList.children.length == 0){
 
 UserGetButton.addEventListener("click", function() {
     var AJAX = new XMLHttpRequest();
-    AJAX.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    AJAX.open('GET', 'https://jsonplaceholder.typicode.com/users' , true);
 
-    AJAX.addEventListener("load", function() {
-        if (AJAX.readyState === XMLHttpRequest.OPENED){
+
+
+    AJAX.addEventListener("loadstart", function () {
+        UserUnorderList.innerHTML = "";
+        ListItemCreator("The request is loading. Please wait...");
+    })
+    AJAX.addEventListener("readystatechange", function() {
+        if (AJAX.readyState === XMLHttpRequest.OPENED) {
             UserUnorderList.innerHTML = "";
-            ListItemCreator("Request sent.")
+            ListItemCreator("Request has been opened.");
         }
-        if (AJAX.readyState === XMLHttpRequest.LOADING){
+        else if (AJAX.readyState === XMLHttpRequest.HEADERS_RECEIVED) {
             UserUnorderList.innerHTML = "";
-            ListItemCreator("The request is in progress. Please wait.")
+            ListItemCreator("The request is loading. Please wait...");
         }
-        if (AJAX.readyState === XMLHttpRequest.DONE){
+        else if (AJAX.readyState === XMLHttpRequest.LOADING) {
+            UserUnorderList.innerHTML = "";
+            ListItemCreator("The request is loading. Please wait...");
+        }
+        else if (AJAX.readyState === XMLHttpRequest.DONE){
             if (AJAX.status === 200) {
                 UserUnorderList.innerHTML = "";
                 var ResultData = JSON.parse(AJAX.responseText);
@@ -39,11 +49,17 @@ UserGetButton.addEventListener("click", function() {
                     ListItemCreator(`[Username: ${object.username}]   [Name: ${object.name}]   [Email: ${object.email}]`);
                 });
             } else {
-                alert("Cannot GET file from server. Status: " + AJAX.status);
+                UserUnorderList.innerHTML = "";
+                ListItemCreator("Cannot GET file from server. Status: " + AJAX.status);
             }
         }else{
-            alert("Request failed. Please try again later.");
+            UserUnorderList.innerHTML = "";
+            ListItemCreator("Request failed. Please try again later.");
         }
+    });
+    AJAX.addEventListener("error", function () {
+        UserUnorderList.innerHTML = "";
+        ListItemCreator("Request failed. Please try again later.");
     });
     AJAX.send();
 });
@@ -51,4 +67,77 @@ UserGetButton.addEventListener("click", function() {
 Resetlist.addEventListener("click" , function () {
     UserUnorderList.innerHTML = "";
     ListItemCreator("List item is null. Please click the button");
+})
+
+
+
+
+
+var PostDataForm = document.querySelector("#PostDataForm");
+var UsernameInput = document.querySelector("#UsernameInput");
+var NameInput = document.querySelector("#NameInput");
+var StatusMessage = document.querySelector("#StatusMessage");
+var SetTimeID;
+
+
+PostDataForm.addEventListener("submit" , function (e) {
+    e.preventDefault();
+    StatusMessage.textContent = "Data is ready to be sent...";
+
+    var usernameVariable = UsernameInput.value;
+    var nameVAriable = NameInput.value;
+    var JSONrequest = JSON.stringify({
+        username: usernameVariable,
+        name: nameVAriable,
+    })
+
+    if (SetTimeID) {
+        clearTimeout(SetTimeID);
+    }
+
+    var AJAX = new XMLHttpRequest();
+    AJAX.open('Post', 'https://jsonplaceholder.typicode.com/users' , true);
+    AJAX.setRequestHeader("Content-Type", "application/json");
+
+
+    AJAX.addEventListener("loadstart", function () {
+        StatusMessage.textContent = "Sending information... Please wait.";
+        StatusMessage.style.color = "blue";
+    });
+
+    AJAX.addEventListener("readystatechange", function () {
+        if (AJAX.readyState === XMLHttpRequest.LOADING) {
+            StatusMessage.textContent = "Sending information... Please wait.";
+            StatusMessage.style.color = "blue";
+        }
+    });
+
+
+    AJAX.addEventListener("load", function() {
+        if (AJAX.readyState === XMLHttpRequest.DONE){
+            setTimeout(function(){
+            if (AJAX.status === 201) {
+                StatusMessage.textContent = "The operation was successful.";
+                StatusMessage.style.color = "green";
+                clearTimeout(SetTimeID);
+            } else {
+                UserUnorderList.innerHTML = "";
+                StatusMessage.textContent = "Cannot Post file to server. Status: " + AJAX.status;
+                StatusMessage.style.color = "red";
+            }
+        } , 2000)
+        }else{
+            UserUnorderList.innerHTML = "";
+            StatusMessage.textContent = "Request failed. Please try again later.";
+            StatusMessage.style.color = "red";
+        }
+    });
+    AJAX.addEventListener("error", function () {
+        UserUnorderList.innerHTML = "";
+        StatusMessage.textContent = "Request failed. Please try again later.";
+        StatusMessage.style.color = "red";
+    });
+
+    SetTimeID = setTimeout(() => StatusMessage.textContent = "",5000)
+    AJAX.send(JSONrequest);
 })
